8fcce22f8dff39f17981dcbb4f94d1e6ac8bd3ff
José Vitor M <110407507+jsvitorm@users.noreply.github.com>
2023-09-29 16:38:54 -0300

    Add files via upload

---
+++ prova2/20232ASET06P2/src/backend/irrf.test.js
@@ -0,0 +1,25 @@
+const irrf = require('./irrf')
+
+describe('[Obter faixas IRRF]', () => {
+    test('deve retornar 05 elementos', () => {
+        const faixas = irrf.obterFaixas();
+        expect(faixas.length).toEqual(5);
+    });
+});
+
+describe('[Encontrar aliquota IRRF]', () => {
+    test('deve retornar zero', () => {
+        const faixas = irrf.obterFaixas();
+        const result = irrf.faixaContemplaValor(100, faixas[0]);
+        expect(result).toEqual(true);
+    });
+});
+
+
+describe('[Calcular desconto IRRF]', () => {
+    test('deve retornar 45.80', () => {
+        const result = irrf.calculardesconto(2722.61);
+        expect(result).toEqual(45.80);
+    });
+    
+});
